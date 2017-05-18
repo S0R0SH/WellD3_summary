2a488baf6f7230b7bb60f685f855ba4174a8f3b5
Eric Booker <ericbooker12@gmail.com>
2017-05-18 03:00:53 -0700

    Create drawLith method.

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -252,85 +252,80 @@ $(document).ready(function(){
 				.attr("class", "annotation")
 				.call(makeAnnotations)
 
-
-		// var lithArr = [];
-
-		// var liths = d.data.attributes.lithologies;
-
-		// console.log('lith', lith)
-
-		// var lithData = lith.forEach(function(d){
-		// 	lithArr.push(formatLithData(d))
-		// })
-
-
-		var lithObj = d.data.attributes.lithologies;
-		var newLith = toLithArray(lithObj);
+		var newLith = toLithArray(d.data.attributes.lithologies);
 		var lith100 = avgLithArray(newLith);
 
+		drawLith(lithColumn, yScale, lithColDimension.width, lith100)
+
 		// Put in lith function
-		var lithSvg = lithColumn.append('svg')
-			.attr('x', 0)
-			.attr('height', yScale(maxDepth))
-			.attr('width', lithColDimension.width)
+		function drawLith(lithColumn, yScale, columnWidth, lith){
 
-		var yOffset = .5;
+			var lithSvg = lithColumn.append('svg')
+				.attr('x', 0)
+				.attr('height', yScale(maxDepth))
+				.attr('width', columnWidth)
 
-		lith100.forEach(function(d){
+			var yOffset = .5;
 
-			var depth = d[0];
-			var syms = d[1];
-			var percents = d[2];
-			var xPosition = 0;
+			var td = d3.max(lith100, function(d) { return d[0]} );
 
-			for (var i = 0; i < percents.length; i++) {
 
-				// append clipPath to svg
-				// give clipPath an id
-				// append rect to clipPath
-				// give rect attrs based on lith data
+			lith100.forEach(function(d){
 
-				lithSvg.append('clipPath')
-					.attr('id', 'clipped')
-					.append('rect')
-						.attr('x', xPosition)
+				var depth = d[0];
+				var syms = d[1];
+				var percents = d[2];
+				var xPosition = 0;
+
+				for (var i = 0; i < percents.length; i++) {
+
+					// append clipPath to svg
+					// give clipPath an id
+					// append rect to clipPath
+					// give rect attrs based on lith data
+
+					lithSvg.append('clipPath')
+						.attr('id', 'clipped')
+						.append('rect')
+							.attr('x', xPosition)
+							.attr('y', function(){
+								if (depth % 100 == 0) {
+									return yScale(depth - 100) + yOffset
+								} else {
+									return yScale((Math.ceil(depth/100) * 100) - 100) + yOffset
+								}
+							})
+							.attr('width', (columnWidth * (percents[i]) / 100) )
+							.attr('height', yScale(100));
+
+					// append svg:image to svg
+					// set x & y to 0
+					// set clip-path, attr to id of rect
+					lithSvg.append('svg:image')
+						.attr('xlink:href', function(){
+							return `/images/lith/${syms[i]}.svg`
+						})
+						// .attr('fill', 'red')
+						// .attr('stroke', 'black')
+						.attr('x', 0)
 						.attr('y', function(){
-							if (depth % 100 == 0) {
-								return yScale(depth - 100) + yOffset
-							} else {
-								return yScale((Math.ceil(depth/100) * 100) - 100) + yOffset
-							}
-						})
-						.attr('width', (lithColDimension.width * (percents[i]) / 100) )
-						.attr('height', yScale(100));
+								if (depth % 100 == 0) {
+									return yScale(depth - 100) + yOffset
+								} else {
+									return yScale((Math.ceil(depth/100) * 100) - 100) + yOffset
+								}
+							})
+						.attr('width', columnWidth)
+						.attr('height', yScale(100))
+						.attr('class', `lith`)
+						.attr('clip-path', 'url(#clipped)')
 
-				// append svg:image to svg
-				// set x & y to 0
-				// set clip-path, attr to id of rect
-				lithSvg.append('svg:image')
-					.attr('xlink:href', function(){
-						return `/images/lith/${syms[i]}.svg`
-					})
-					// .attr('fill', 'red')
-					// .attr('stroke', 'black')
-					.attr('x', 0)
-					.attr('y', function(){
-							if (depth % 100 == 0) {
-								return yScale(depth - 100) + yOffset
-							} else {
-								return yScale((Math.ceil(depth/100) * 100) - 100) + yOffset
-							}
-						})
-					.attr('width', lithColDimension.width)
-					.attr('height', yScale(100))
-					.attr('class', `lith`)
-					.attr('clip-path', 'url(#clipped)')
+					xPosition += columnWidth * (percents[i])/100
 
-				xPosition += lithColDimension.width * (percents[i])/100
-				// end of lith function
-
-		 }
-	 })
+				}
+			})
+		}
+		// end of lith function
 
 		var min = d.data.attributes.mineralogies;
 
